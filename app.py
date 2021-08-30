333e67ebb28891002aa0ec7c5818378165180dc4
Anuganti Suresh <53964100+sureshmecad@users.noreply.github.com>
2021-08-30 16:10:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import joblib
 import os
 import numpy as np
 
-st.set_page_config(page_title="ADROIT",
+st.set_page_config(page_title="HIGGS BOSON",
                    page_icon="📵", layout="wide")
 
 curr_path = os.path.dirname(os.path.realpath(__file__))
@@ -16,29 +16,26 @@ feature_cols = joblib.load(curr_path + "/features.joblib")
 with st.form("prediction_form"):
     st.header("Enter the Details about App")
 
-    Rating = st.number_input("Rating of APP: ")
-    NumberofRatings = st.number_input("Total Ratings: ", value=0, format="%d")
-    Price = st.number_input("App Price: ")
-    Safe = st.number_input("Number of safe permissions: ", value=0, format="%d")
-    Dangerous = st.number_input("Number of Dangerous permissions:", value=0, format="%d")
-    permission = st.multiselect("Permit the app:",
-                                feature_cols[5:35]) 
-    Category = st.selectbox("Permit the app:",
-                                feature_cols[35:])
-    submit_val = st.form_submit_button("Predict Malware")
-
+    DER_mass_MMC = st.number_input("DER_mass_MMC: ")
+    DER_mass_transverse_met_lep = st.number_input("DER_mass_transverse_met_lep: ", value=0, format="%d")
+    DER_mass_vis = st.number_input("DER_mass_vis: ")
+    DER_deltar_tau_lep = st.number_input("DER_deltar_tau_lep: ", value=0, format="%d")
+    DER_met_phi_centrality = st.number_input("DER_met_phi_centrality:", value=0, format="%d")
+    DER_pt_h = st.number_input("DER_pt_h:", value=0, format="%d")
+    PRI_met = st.number_input("PRI_met:", value=0, format="%d")
+    submit_val = st.form_submit_button("Classify")
 
 
 if submit_val:
-    per_d = dict(zip(feature_cols[5:35],np.zeros(30)))
+    per_d = dict(zip(feature_cols[0:11],np.zeros(30)))
     for p in permission:
         per_d[p] = 1
     permission = np.array(list(per_d.values()))
-    cat = dict(zip(feature_cols[35:], np.zeros(15)))
+    cat = dict(zip(feature_cols[0:], np.zeros(15)))
     cat[Category] = 1
     Category = np.array(list(cat.values()))
 
-    base_feats = np.array([Rating, NumberofRatings, Price, Dangerous, Safe])
+    base_feats = np.array([DER_mass_MMC, DER_mass_transverse_met_lep, DER_mass_vis, DER_deltar_tau_lep, DER_met_phi_centrality, DER_pt_h, PRI_met])
 
     attributes = np.concatenate([base_feats, permission, Category])
     
@@ -47,7 +44,7 @@ if submit_val:
     status = predict(attributes.reshape(1, -1))
 
     if status:
-        st.error("The App is Malware")
+        st.error("The App is Signal")
     else:
-        st.success("The App is Benign")
+        st.success("The App is Background")
         st.balloons()
